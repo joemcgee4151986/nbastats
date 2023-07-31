d831f5db4d1fc0f87628a9d9140ac86c6750e595
unknown <jrmcgee@asu.edu>
2023-07-31 17:21:08 +0900

    debugging cards to properly accept pictures

--- src/components/body/BaseStats.vue
+++ src/components/body/BaseStats.vue
@@ -28,26 +28,25 @@
 
 </v-card>
 
+
 <v-card id = "stats-number">
-  <template v-slot:title>
-  2
-</template>
+  
 
 </v-card>
 <v-card id = "stats-player">
   
-  <img src="@/assets/hardenpic.jpeg" id = "player-pics">
 
 
 </v-card>
+
+
+
 <v-card id = "stats-names">
   <template v-slot:title>
-  James Harden
+  
   </template>
 </v-card>
-
 </template>
-
 <style>
 #stats1{
     display: inline-flex;
@@ -56,31 +55,38 @@
   height: 150px;
   background-color:white;
   }
+  #stats2{
+    display: inline-flex;
+  flex-direction: column;
+  width: 25%;
+  height: 150px;
+  background-color:white;
+  
+  }
+
 
   #stats-number{
     display: inline-flex;
   flex-direction: column;
-  width: 3%;
-  height: 80px;
+  width: 25%;
+  height: 150px;
   background-color:white;
+ 
   }
   #stats-player{
     display: inline-flex;
   flex-direction: column;
-  width: 6%;
+  width: 10%;
   height: 100px;
   background-color:white;
+  content:url("@/assets/hardenpic.jpeg");
   }
   #stats-names{
     display: inline-flex;
   flex-direction: column;
-  width: 16%;
-  height: 80px;
+  width: 25%;
+  height: 150px;
   background-color:white;
-  border: solid;
   }
-  #player-pics{
-    width: 100px;
-  height: 90px;
-  }
+ 
 </style>
